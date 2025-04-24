1acd9327cc24c5cfd06f5663711717e4e568aef2
Jorge Villaplana Millá <jorge.villaplana.m@gmail.com>
2025-04-24 13:09:22 +0200

    Spring boot project in directory

---
+++ backend/taskreward/src/main/java/com/rewardtme/taskreward/TaskRewardApplication.java
@@ -0,0 +1,13 @@
+package com.rewardtme.taskreward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRewardApplication.class, args);
+	}
+
+}
---
+++ backend/taskreward/src/test/java/com/rewardtme/taskreward/TaskRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewardtme.taskreward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
